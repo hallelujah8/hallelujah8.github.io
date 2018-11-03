3d95a185b7a774c0e6fecd9ac646624331617b25
hallelujah8 <37848825+hallelujah8@users.noreply.github.com>
2018-11-03 18:05:43 +0300

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,45 @@
+<?php
+	echo 'fuck';
+	$dir    = glob('/dir/');
+	echo '<pre>';
+	print_r($dir);
+	
+	
+	/*
+	$files1 = opendir($dir);
+	$files2 = scandir($dir, 1);
+	
+	while (false !== ($filename = readdir($files1))) 
+	{
+		$files[] = $filename;
+	}	
+
+	sort($files);
+
+	print_r($files);
+
+	rsort($files);
+
+	print_r($files);
+
+	print_r($files1);
+	print_r($files2);
+	echo $files1[0];
+	
+/*if ($handle = opendir('/path/to/files')) {
+    echo "Дескриптор каталога: $handle\n";
+    echo "Файлы:\n";
+
+    ///* Именно этот способ чтения элементов каталога является правильным. 
+    while (false !== ($file = readdir($handle))) { 
+        echo "$file\n";
+    }
+
+    //* Этот способ НЕВЕРЕН. 
+    while ($file = readdir($handle)) { 
+        echo "$file\n";
+    }
+
+    closedir($handle); 
+}*/
+?>
